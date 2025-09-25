911b9b4fd91bc6021ff987609fb386049a9517aa
Hoang Hung <hhungln28@gmail.com>
2025-09-25 14:08:57 +0700

    Initial of lab2

---
+++ Lab2/Exercise/Core/Src/sofwar_timer.c
@@ -0,0 +1,25 @@
+/*
+ * sofwar_timer.c
+ *
+ *  Created on: Sep 25, 2025
+ *      Author: Hoàng Hưng
+ */
+
+#include "software_timer.h"
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+void setTimer1(int duration){
+	timer1_counter = duration;
+	timer1_flag = 0;
+}
+
+void timerRun(){
+	if(timer1_counter > 0){
+		timer1_counter--;
+		if(timer1_counter <= 0){
+			timer1_flag = 1;
+		}
+	}
+}
